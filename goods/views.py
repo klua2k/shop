393304c961e8b845ee092d79431e8c100b9c4357
klua <expl@gmail.com>
2024-03-03 18:36:26 +0500

    3-rd commit admin

--- goods/views.py
+++ goods/views.py
@@ -2,10 +2,34 @@ from django.shortcuts import render
 
 import goods
 
+
 # Create your views here.
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        "title": "Магазин - Каталог",
+        "goods": [
+            {
+                "image": "deps/images/goods/t-shirt.jpg",
+                "name": "Белая футболка",
+                "description": "Базовая белая футболка.",
+                "price": 150.00,
+            },
+            {
+                "image": "deps/images/goods/jeans1.jpg",
+                "name": "Джинсы",
+                "description": "Карго.",
+                "price": 93.00,
+            },
+            {
+                "image": "deps/images/goods/shoes.jpg",
+                "name": "Белые кроссовки",
+                "description": "Повседневные белые кроссовки.",
+                "price": 670.00,
+            },
+        ],
+    }
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html")
