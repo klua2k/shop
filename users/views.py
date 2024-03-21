25dc81e401ed0587ea22f3ede16a0fac12162628
klua <expl@gmail.com>
2024-03-21 19:24:22 +0500

    login ready

--- users/views.py
+++ users/views.py
@@ -1,12 +1,33 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 # Create your views here.
 def login(request):
+    # при нажатие я возвращаю обычный GET ЗАПРОС form = UserLoginForm(), а если работает на странице то POST
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            # Формируем клиента и проверяем есть ли он в БД
+            user = auth.authenticate(username=username,password = password)
+            if user:
+                auth.login(request,user)
+                # Перенаправляем на главную страницу
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context ={
-        'title': 'Магазин - Авторизация'
+        'title': 'Магазин - Авторизация',
+        'form': form
     }
     # Передаем рек, html шаблон и context 
-    return render(request, 'users/login.html',context)
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     context ={
